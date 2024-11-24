b2b914abd0a568609c1170936e29227bb593b35a
Leeyoji <leojay.eulatriz@wvsu.edu.ph>
2024-11-24 15:36:19 +0800

    setting up dependencies

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider"
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
+}
