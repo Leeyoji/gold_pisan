b9725af4874a50404a72534372886f0b0986d5de
Leeyoji <leojay.eulatriz@wvsu.edu.ph>
2024-11-24 15:58:48 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider"
+import { ReactNode } from "react"; // Import ReactNode
+
+// Define the type for RootLayoutProps
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
